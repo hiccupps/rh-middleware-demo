8bfc8510de40fa5f9adb9637c2c8617047f8ac80
Abhishek Vishnoi <abvishno@redhat.com>
2023-06-18 15:16:50 +0530

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM openjdk:11-jdk-buster
+#FROM registry.redhat.io/fuse7/fuse-java-openshift-rhel8
 ARG JAR_FILE=target/*.jar
 COPY ${JAR_FILE} app.jar
+CMD ls -lah
 #ENTRYPOINT ["java","-jar","/app.jar"]
 ENTRYPOINT ["java","-jar","/app.jar" , "--spring.config.location=file:///etc/config/application.properties"]
+CMD ["-start"]
--- src/main/java/org/mycompany/MyRoute.java
+++ src/main/java/org/mycompany/MyRoute.java
@@ -12,7 +12,6 @@ public class MyRoute extends RouteBuilder {
 	 @Override
 	    public void configure() throws Exception {
 
-
 	    restConfiguration()
          .enableCORS(true)
          .component("jetty")
@@ -20,24 +19,33 @@ public class MyRoute extends RouteBuilder {
          .port(8988)
          .bindingMode(RestBindingMode.json);
 
+		rest()
+         .get("/hello-sds")
+         .to("direct:hello-sds");
+
+     from("direct:hello-sds")
+     .routeId("GreetingSamsungSDSRoute")
+		 .to("direct:say-hello")
+     	 .to("direct:produce-to-sds-kafka");
 
 
-      rest()
-         .get("/hello")
-         .to("direct:hello");
-
-
-     from("direct:hello")
-     .routeId("GreetingRoute")
-     	 .to("direct:greetStranger");
-
-
-     from("direct:greetStranger")
+     from("direct:say-hello")
 	  	.routeId("greetStranger")
-	  	.setBody(simple("Hello Application!"));
+	  	.setBody(simple("Hello Samsung!!"));
 
+	 from("direct:produce-to-sds-kafka")
+			 .routeId("KafkaProducerGreetingRoute")
+			 .setBody(simple("Hello Kafka !"))
+			 .log("Sednign Message to Kafka : ${body}")
+			 .to("kafka:{{topic}}?brokers={{broker}}");
 
-
+	 from("kafka:{{topic}}?brokers={{broker}}")
+			 .routeId("KafkaConsumerGreetingRoute")
+			 .log("Message received from Kafka : ${body}")
+			 .log("    on the topic ${headers[kafka.TOPIC]}")
+			 .log("    on the partition ${headers[kafka.PARTITION]}")
+			 .log("    with the offset ${headers[kafka.OFFSET]}")
+			 .log("    with the key ${headers[kafka.KEY]}");
 
 
 
